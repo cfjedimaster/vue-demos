75af8d81207b06bcabfc9eb9e8a6693d045c76c7
Raymond Camden <raymondcamden@gmail.com>
2019-09-08 07:30:33 -0500

    typo

--- nps_gallery/src/views/Home.vue
+++ nps_gallery/src/views/Home.vue
@@ -2,7 +2,7 @@
   <v-container>
       <v-row>
         <v-col cols="2" v-for="(state, abbr) of states" :key="state">
-          <v-btn color="teal" width="100%" :to="{name:'state',params:{state:state, abbr:abbr}}"> {{ state}}</v-btn>
+          <v-btn color="teal" width="100%" :to="{name:'state',params:{state:state, abbr:abbr}}">{{ state}}</v-btn>
         </v-col>
         <!--
         <v-btn v-for="(state,abbr) in states" :key="state" color="teal" width="250"> {{ state }}</v-btn>
