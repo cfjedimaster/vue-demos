42815cc71981a4a8c2347f695a36547447d9e5b4
Raymond Camden <raymondcamden@gmail.com>
2019-12-08 09:18:42 -0600

    eliza ftw

---
+++ eliza/js/app.js
@@ -0,0 +1,23 @@
+const app = new Vue({
+	el:'#app',
+	data: {
+		chat:'',
+		msg:'',
+		eliza:null
+	},
+	created() {
+		this.eliza = new ElizaBot();
+		this.chat = 'Eliza: '+this.eliza.getInitial();
+	},
+	methods: {
+		speak() {
+			let reply = this.eliza.transform(this.msg);
+			this.chat += `<br/>You: ${this.msg}<br/>Eliza: ${reply}`;
+			this.msg = '';
+			this.$nextTick(() => {
+				// https://stackoverflow.com/a/40737063/52160
+				this.$refs.chatBox.scrollTop = this.$refs.chatBox.scrollHeight;
+			});
+		}
+	}
+});
