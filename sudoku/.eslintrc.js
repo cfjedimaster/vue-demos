dec28c205975c77fde25152623a768e835a94319
Raymond Camden <raymondcamden@gmail.com>
2019-12-16 20:39:19 -0600

    remove damn console lint

--- sudoku/.eslintrc.js
+++ sudoku/.eslintrc.js
@@ -4,12 +4,9 @@ module.exports = {
     node: true
   },
   'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
+    'plugin:vue/essential'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
