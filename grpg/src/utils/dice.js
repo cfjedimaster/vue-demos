345420cfafaff81f1e6e195f91a4168dcb0b2326
Raymond Camden <raymondcamden@gmail.com>
2020-07-17 14:41:05 -0500

    improvement to dice to support bonuses

--- grpg/src/utils/dice.js
+++ grpg/src/utils/dice.js
@@ -4,14 +4,25 @@ export const dice = {
 	xdy
 
 	x rolls of a y sided dice
+
+	xdy+n
+	xdy
+
+	x rolls of a y side dice + n
 	*/
 	roll(style) {
+		let bonus=0, total=0;
+		if(style.indexOf('+') > -1) {
+			[style, bonus] = style.split('+');
+		} 
+		
 		let [rolls, sided] = style.split('d');
-		console.log(rolls, sided);
-		let total = 0;
+		
+		//console.log(rolls, sided);
 		for(let i=0;i<rolls;i++) {
 			total += getRandomIntInclusive(1, sided);
 		}
+		total += parseInt(bonus);
 		return total;
 	}
 }
